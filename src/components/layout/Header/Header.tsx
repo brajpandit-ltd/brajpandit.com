263d775890dbe9c8a912e6930944ba3a12b99645
JaYRaNa213 <tresurejay6@gmail.com>
2025-10-05 20:02:33 +0530

    header button small

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -39,7 +39,7 @@ const Header = () => {
         />
 
         <Link href="/pandits">
-          <Button variant="primary" label="Book Your Pooja" size="medium" />
+          <Button variant="primary" label="Book Your Pooja" size="small" />
         </Link>
       </nav>
 
